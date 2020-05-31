5b34a767c2b8a432d6708aed6f055603bb651ef4
Marquinhos <Marquinhos.jesus44@gmail.com>
2020-05-30 21:41:38 -0300

    commit

--- Projeto_Xadrez/Program.cs
+++ Projeto_Xadrez/Program.cs
@@ -8,11 +8,12 @@ namespace Projeto_Xadrez
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main(string[] args){
             Console.WriteLine("Projeto Jogo Xadrez");
             Console.WriteLine("Vamos Inciar O Projeto");
-            
+            Console.WriteLine("A marcela é chata");
+           
+
             Console.ReadLine();
         
         }
